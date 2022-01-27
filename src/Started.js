f76b4b05befd7fc609aa4362548e33a68a8f9485
Daramola femi <dfmopeoluwa@gmail.com>
2022-01-27 15:06:20 +0100

    get started component created

---
+++ src/Started.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+function Started() {
+    return (
+        <div className="started">
+            <h1 className="started__Title">Get started today</h1>
+
+            <form action="">
+                <input type="text" name="wallet_id" id=""/>
+                <input type="submit" value="connect"/>
+            </form>
+        </div>
+    );
+}
+
+export default Started;
