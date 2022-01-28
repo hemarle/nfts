02f2280d0b2534e3b51730e024c039bf41d56bb6
Daramola femi <dfmopeoluwa@gmail.com>
2022-01-28 14:03:58 +0100

    services component created

--- src/Services.js
+++ src/Services.js
@@ -1,7 +1,23 @@
 import React from 'react';
 import './Services.css'
+import ServicesCard from './ServicesCard';
 function Services() {
-    return <div className='services'></div>;
+    let servicesData = [{
+            image: '',
+            title: 'CEX',
+            about: 'lorem10px',
+            link: '/cex'
+        }]
+    return <div className='services'>
+        {
+        servicesData.map(({image, title, about, link}) => {
+            <ServicesCard image={image}
+                title={title}
+                about={about}
+                link={link}/>
+
+        })
+    } </div>
 }
 
 export default Services;
