d39a2715b74024c6a80488559581b6318d1fc03c
Daramola femi <dfmopeoluwa@gmail.com>
2022-01-28 13:58:40 +0100

    services card component created

--- src/About.js
+++ src/About.js
@@ -3,28 +3,24 @@ import "./About.css";
 import phoneIcon from "./images/phone.png";
 import AboutCard from "./AboutCard";
 function About() {
-  return (
-    <div className="about">
-      <h1 className="about__Title">Explore -> Buy -> Manage</h1>
+    return (<div className="about">
+        <h1 className="about__Title">Explore -> Buy -> Manage</h1>
 
-      <div className="about__Cards">
-        <AboutCard
-          title="Explore the metaverse from one place"
-          about="From DeFi tokens to NFTs, it’s never been easier to search, filter and evaluate every asset on the market. See what’s trending, compare historical returns, and more."
-        image={phoneIcon}
-        link={'/explore'}
-        linkText='Explore'
-        />
-        <AboutCard
-          title="Explore the metaverse from one place"
-          about="From DeFi tokens to NFTs, it’s never been easier to search, filter and evaluate every asset on the market. See what’s trending, compare historical returns, and more."
-        image={phoneIcon}
-        link={'/explore'}
-        linkText='Explore'
-        />
-      </div>
+        <div className="about__Cards">
+            <AboutCard title="CMEX" about="Community exchange done with ease"
+                image={phoneIcon}
+                link={'/cmex'}
+                linkText='Explore'/>
+
+
+            <AboutCard title="DEX" about="Nothing is better than Decentralised exchange. Take advantage of our DEX program and get the best we have to offer"
+                image={phoneIcon}
+                link={'/explore'}
+                linkText='Explore'/>
+
+        </div>
     </div>
-  );
-}
-
-export default About;
+    );
+            }
+            
+            export default About;
---
+++ src/ServicesCard.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './ServicesCard.css'
+function ServicesCard({image, title, about, link}) {
+    return <div className='servicesCard'>
+        <img src={image}
+            alt=""/>
+        <h3>{title}</h3>
+        <p>{about}</p>
+        <a href={link}>
+            Explore -></a>
+    </div>
+;
+}
+
+export default ServicesCard;
