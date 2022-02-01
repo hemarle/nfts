116263e5c48008001240e6a32a6674d62f5f768e
Daramola femi <dfmopeoluwa@gmail.com>
2022-02-01 16:21:45 +0100

    Naavigaation removed

--- src/App.js
+++ src/App.js
@@ -1,12 +1,11 @@
 import "./App.css";
-import Navigation from './Navigation'
 
 import Footer from './Footer'
 import Landing from "./Landing";
 function App() {
     return (
         <div className="app">
-            <Navigation/>
+
             <Landing/>
             <Footer title='AcenDAO' about='Transaction has never been this easy. Be in charge of all your transactions today'/>
         </div>
